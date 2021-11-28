f449e005666224d8f3c1a16e4194a1cf816e4791
gedoyamusa <91181196+gedoyamusa@users.noreply.github.com>
2021-11-28 23:49:59 +0000

    changes

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require("express");
 //creating app
 const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.sendFile("public/index.html", { root: __dirname });
+  res.render("index", { root: __dirname });
 });
-app.get("/about-us", (req, res) => {
-  res.sendFile("public/about.html", { root: __dirname });
+app.get("/contacts", (req, res) => {
+  res.render("contacts");
 });
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+app.post("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/catalogue", (req, res) => {
+  res.render("catalogue");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+const router = require("./routes/bantz");
+app.use(router);
+
+// app.get("/about-us", (req, res) => {
+//   res.sendFile("public/about.html", { root: __dirname });
+// });
 
 //app.post("/register", (req,res))
 //make the app listen on port
@@ -14,3 +37,11 @@ const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Cart app listening at http://localhost:${port}`);
 });
+
+/*
+Basic HTML
+Basic to some CSS
+Lots of JavaScript - Node.js (Server)
+Algorithm and Problem Solving
+Data Structure, SQL Queries
+*/
